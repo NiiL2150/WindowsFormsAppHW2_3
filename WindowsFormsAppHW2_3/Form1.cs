e374f6def7bb550eb27c28099195b5b072892a53
NiiL2150 <nail.shamsudinov@gmail.com>
2021-06-27 15:11:11 +0400

    Add project files.

---
+++ WindowsFormsAppHW2_3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppHW2_3
+{
+    public partial class Form1 : Form
+    {
+        public string filePath = String.Empty;
+        public string initialText = String.Empty;
+        public int opened = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = $"All files(*.*)|*.*| Text files (*.txt)|*.txt"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog.FileName);
+                filePath = openFileDialog.FileName;
+                initialText = sr.ReadToEnd();
+                textBox1.Text = initialText;
+                button2.Enabled = true;
+                label1.Text = filePath;
+                sr.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            opened++;
+            Form2 form = new Form2();
+            if (form.Show(this) == DialogResult.OK)
+            {
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = initialText;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = (opened != 0);
+        }
+    }
+}
---
+++ WindowsFormsAppHW2_3/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppHW2_3
+{
+    public partial class Form2 : Form
+    {
+        Form1 parentForm = null;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public DialogResult Show(Form1 form)
+        {
+            parentForm = form;
+            textBox1.Text = parentForm.initialText;
+            base.Show();
+            return this.DialogResult;
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            parentForm.opened--;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            parentForm.initialText = textBox1.Text;
+            parentForm.textBox1.Text = textBox1.Text;
+            StreamWriter sw = new StreamWriter(parentForm.filePath);
+            sw.Write(textBox1.Text);
+            sw.Close();
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
